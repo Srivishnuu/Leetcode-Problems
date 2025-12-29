490a4106a7cf98b1797af4c14071e7eae5a01dd9
Srivishnu Murugavel <113229637+Srivishnuu@users.noreply.github.com>
2025-12-29 21:46:03 +0530

    Time: 29 ms (99.18%) | Memory: 17.2 MB (99.93%) - LeetSync

---
+++ 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findLength(self, nums1: List[int], nums2: List[int]) -> int:
+        nums2_str="".join([chr(i) for i in nums2])
+        max_str = ""
+        res=0
+        for i in nums1:
+            max_str +=chr(i)
+            if max_str in nums2_str:
+                res= max(res,len(max_str))
+            else:
+                max_str=max_str[1:]
+        return res
+       
+
+
+        
